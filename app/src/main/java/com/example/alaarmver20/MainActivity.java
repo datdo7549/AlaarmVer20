f830b1a0ca8d3c2b724cddc8d1f427e390066ea5
ThanhDat <datdo7549@gmail.com>
2019-11-06 07:56:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/alaarmver20/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.alaarmver20;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import java.util.Calendar;
+
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextInput;
+    private TimePicker time;
+    private PendingIntent pendingIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextInput = findViewById(R.id.edit_text_input);
+    }
+
+    public void startService(View v) {
+        String input = editTextInput.getText().toString();
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+
+    public void stopService(View v) {
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        stopService(serviceIntent);
+    }
+}
